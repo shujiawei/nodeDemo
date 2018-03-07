ddd2a9406543eb14892382c72b2474cb367d8e4e
苏家为 <sjw@myweiju.com>
2018-03-07 18:08:05 +0800

    模仿登陆完成

--- login.js
+++ login.js
@@ -2,9 +2,9 @@ const http = require('http')
 const fs = require('fs')
 const querystring = require('querystring')
 const urlLib = require('url')
-
+var users ={};
 http.createServer((req, res)=> {
-	let str,users ={};
+	let str;
 	req.on('data',data => {
 		str += data
 	})
@@ -14,25 +14,40 @@ http.createServer((req, res)=> {
 	const obj = urlLib.parse(req.url,true)
 	const url = obj.pathname
 	const get = obj.query
-	if(req.url == '/user') {
+	if(url == '/user') {
 		if(get.act == 'reg') {
+			console.log(users[get.user])
 			if(users[get.user]){
-				rew.write('该账号已经注册了')
+				res.write("{'ok':false,'message':'该账号已经注册了'}")
 			}else{
-				users['user'] = user[get.user]
-				users['pass'] = user[get.pass]
-				rew.write('注册成功')
+				users[get.user] = get.pass
+				console.log(users[get.user])
+				res.write("{'ok':true,'message':'注册成功'}")
 			}
 		}else{
 			if(users[get.user]){
 				if(users[get.pass] == get.pass){
-					res.write('登陆成功')
+					res.write("{'ok':true,'message':'登陆成功'}")
 				}else{
-					res.write('密码错误')
+					res.write("{'ok':false,'message':'密码错误'}")
 				}
 			}else{
-				res.write('密码错误')
+				res.write("{'ok':false,'message':'密码错误'}")
 			}
 		}
+		res.end()
+	}else{
+		var file_name = './www'+req.url
+		if(req.url != '/favicon.ico') {
+			fs.readFile(file_name,(err,data)=>{
+				if(err){
+					console.log(err)
+				}else{
+					res.write(data)
+					res.end()
+				}
+			})
+		}
+		
 	}
 }).listen(8097)
